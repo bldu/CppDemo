874dd348f5714a04902ca982003bb2c1bd6c565f
Bolun Du <Bolun.Du@amlogic.com>
2025-03-07 10:00:00 +0800

    modified

--- test/test_overload_operator.cpp
+++ test/test_overload_operator.cpp
@@ -6,5 +6,7 @@ int main(int argc, char const *argv[])
     OPS ops;
     ops << 1 << 2 << 3 << 4;
     std::cout << ops << std::endl;
+    ops << 7;
+    std::cout << ops << std::endl;
     return 0;
 }
